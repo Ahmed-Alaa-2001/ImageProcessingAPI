9b3b0c6f7cc9d878ea6c5bdea3477b93002abf35
Ahmed Alaa <ahmed2262001.6@gmail.com>
2022-10-19 16:23:24 +0200

    save

--- src/tests/Index.Spec.ts
+++ src/tests/Index.Spec.ts
@@ -1,7 +1,7 @@
 import { valid } from "../Controller/ImageController";
 describe('Test if Image Exist', () => {
     it('expect checkIfImagesExist function to return false with file name imageName_500_500.jpg ', () => {
-        expect(valid(500, 5000, 'imageName') instanceof Promise).toBe(true)
+        expect(valid(500, 500, 'imageName') instanceof Promise).toBe(true)
     })
     it('expect valid function to be defined ', () => {
         expect(valid).toBeDefined()
